939459e942b8813703af2c697e5e5def503f5726
Miguel Angel Garcia Vidal <137082425+MIGUELAGV02@users.noreply.github.com>
2025-02-08 12:54:53 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          xdddddddddddddd
+          xdddddddddddddd dev
         </p>
       </header>
     </div>
